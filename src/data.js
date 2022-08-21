9367a2632b16a2ac8a73d04473aafd6e3aeb4748
cphthomas <cphthomas@gmail.com>
2022-08-21 11:00:41 +0200

    22 podcasts

--- src/data.js
+++ src/data.js
@@ -62,7 +62,7 @@ function chillHop() {
       active: false,
     },
     {
-      name: "6.1 Type 1 og type 2 fejl",
+      name: "7. Type 1 og type 2 fejl",
       cover:
         "https://storage.googleapis.com/tepedu/images/reel2.png",
       artist: "",
@@ -72,7 +72,7 @@ function chillHop() {
       active: false,
     },
      {
-      name: "7. Konfidensinterval 1 andel",
+      name: "8. Konfidensinterval 1 andel",
       cover:
         "https://storage.googleapis.com/tepedu/images/reel2.png",
       artist: "",
@@ -82,7 +82,7 @@ function chillHop() {
       active: false,
     },
     {
-      name: "7.1 Test 1 andel",
+      name: "9. Test 1 andel",
       cover:
         "https://storage.googleapis.com/tepedu/images/reel2.png",
       artist: "",
@@ -92,21 +92,141 @@ function chillHop() {
       active: false,
     },
      {
-      name: "8.",
+      name: "10. Test af 2 andele",
       cover:
         "https://storage.googleapis.com/tepedu/images/reel2.png",
       artist: "",
-      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/2._Variable.m4a",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/8._Test_af_2_andele.m4a",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
     },
      {
-      name: "9.",
+      name: "11. Test af 2 middelværdier",
       cover:
         "https://storage.googleapis.com/tepedu/images/reel2.png",
       artist: "",
-      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/2._Variable.m4a",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/9._Test_af_2_middelv%C3%A6rdier.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+    {
+      name: "12. Korrelation",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/10._Korrelation.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "13. Simpel regression",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/11.1_Simpel_line%C3%A6r_regression.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "14. Multipel lineær regression",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/11.2_Multipel_line%C3%A6r_regression.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "15. Lineær regression forudsætninger",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/11.3_Line%C3%A6r_regression_foruds%C3%A6tninger.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "16. Stikprøver",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/15._Stikpr%C3%B8veudv%C3%A6lgelse.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "17. Chi i anden tests",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/17.1_Chi_i_anden_tests.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "18. Goodness-of-fit tests",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/17.2_Goodness_of_fit_test.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "19. ANOVA",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/18._ANOVA.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "20. Faktoranalyse",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/20._Faktoranalyse.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "21. Klyngeanalyse",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/21._Klyngeanalyse.m4a",
+      color: ["#205950", "#2ab3bf"],
+      id: uuidv4(),
+      active: false,
+    },
+
+     {
+      name: "22. ARIMA",
+      cover:
+        "https://storage.googleapis.com/tepedu/images/reel2.png",
+      artist: "",
+      audio: "https://storage.googleapis.com/tepedu/statistik_podcasts/22._ARIMA.m4a",
       color: ["#205950", "#2ab3bf"],
       id: uuidv4(),
       active: false,
